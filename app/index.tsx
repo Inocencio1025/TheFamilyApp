f591ed0c3cbe8d02af7ef9909e95b2cab315ab1d
R. Inocencio <Inocencio@Oakland.edu>
2025-05-04 21:08:49 -0400

    Annoying ass update to expo 53.

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import { useFonts } from 'expo-font';
 import AppNavigator from './navigation/AppNavigator';
-import { Text } from 'react-native';
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    'PixelSans': require('../assets/fonts/PixelifySans-Regular.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return <Text>Loading...</Text>; // Show a loading message while fonts are loading
-  }
-
-  return (
-    <AppNavigator />
-  );
+  return(
+    <AppNavigator/>
+  )
 }
